f84072bd7eb0885a8ed202b7dfcb06392ed07d2b
Artem-1-1 <aleksejchukartem@gmail.com>
2026-02-24 20:54:17 +0300

    Added form for new-post and style for her.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.route("/").get((req, res) => {
   res.render("index", { title: "Members Only" });
 });
 
+router.route("/new-post").get((req, res) => {
+  res.render("new-post", { title: "Members Only | New Post" });
+});
+
 export default router;
